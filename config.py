1a71ceaa60e53145c658ffb7f08f347ab69f38ff
Che Flowers <crf2127@columbia.edu>
2019-03-02 16:55:25 -0500

    cofig check

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class BaseConfig(object):
 	DEBUG = False
 	SECRET_KEY = "laugh_god_damnit"
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-	#SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/laugh_tracker'
 
 class DevelopmentConfig(BaseConfig):
 	DEBUG = True
