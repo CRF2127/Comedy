7e3ceb7ea2c33b7f0891e514a1c71b99b05474ff
Che Flowers <crf2127@columbia.edu>
2019-03-02 16:44:14 -0500

    updates to db conx

--- routes.py
+++ routes.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yltvamaeksflmr:107bfa9a1e5096f4fe0e9b77fc231ebbcdc238f33e85bcd8a36f2765b6a77fe8@ec2-23-21-244-254.compute-1.amazonaws.com:5432/dc344enjr2hp39'
+# CONFIG
+app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object('config.ProductionConfig')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/laugh_tracker'
+#'postgres://yltvamaeksflmr:107bfa9a1e5096f4fe0e9b77fc231ebbcdc238f33e85bcd8a36f2765b6a77fe8@ec2-23-21-244-254.compute-1.amazonaws.com:5432/dc344enjr2hp39'
 
 db = SQLAlchemy(app)
 
@@ -23,4 +28,4 @@ def index():
 	return render_template("index.html", shows=shows)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
